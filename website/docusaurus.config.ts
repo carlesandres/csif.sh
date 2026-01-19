8083d2d7672158ce43b6bcf55b4ca04247685ba6
Carles Andrés <carlos.andres.c@gmail.com>
2026-01-19 14:49:25 +0000

    Fix Docusaurus baseUrl for GitHub Pages (#8)

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -11,8 +11,8 @@ const config: Config = {
     v4: true,
   },
 
-  url: 'https://csif.sh',
-  baseUrl: '/',
+  url: 'https://carlesandres.github.io',
+  baseUrl: '/csif.sh/',
 
   organizationName: 'carlesandres',
   projectName: 'csif.sh',
